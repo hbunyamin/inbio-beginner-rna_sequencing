95f3263d9018a58266dd85941235f77d53354ab7
Hendra Bunyamin <hendra.bunyamin@it.maranatha.edu>
2026-07-12 22:47:17 +0700

    Hampir selesai

--- codes/mission01-data_preparation.R
+++ codes/mission01-data_preparation.R
@@ -1,3 +1,5 @@
+# Mission 01 : Data Preparation
+# --------------------------------------------------------
 library(DESeq2)
 library(ggplot2)
 library(pheatmap)
@@ -157,3 +159,152 @@ ggplot(pca_df, aes(x=PC1, y=PC2, colour = condition)) +
         x = paste0("PC1:", round(var_explained[1], 1), "% variance"),
         y = paste0("PC2:", round(var_explained[2], 1), "% variance"))+
   theme(legend.position = "bottom")      
+
+
+# Mission 02 : Normalization
+# --------------------------------------------------------
+# =============================
+#   Langkah 1: Normalisasi
+# =============================
+# 1. Load library
+library(DESeq2)
+
+# 2. Buat DESeq2 object (dari count matrix dan metadata)
+dds <- DESeqDataSetFromMatrix(
+  countData = counts,
+  colData = metadata,
+  design = ~condition
+)
+
+# 3. Mulai normalisasi
+dds <- DESeq(dds)
+
+# 4. Ambil size factors (faktor normalisasi)
+sizeFactors(dds)
+
+# 5. Ambil data yang sudah dinormalisasi
+# Data ini bisa kita gunakan untuk visualisasi (heatmap, PCA)
+rld <- rlog(dds, blind = FALSE) # -> Jika dataset kecil, karena lebih lambat
+vsd <- vst(dds, blind = FALSE)  # -> Jika dataset besar, karena lebih cepat
+
+# =====================================
+#   Langkah 2: Persiapan Visualisasi
+# =====================================
+# 1. Ubah data normalisasi (rlog) ke format panjang
+rld_data <- assay(rld)
+rld_long <- rld_data %>%
+  as.data.frame() %>%
+  mutate(gene=rownames(.)) %>%
+  pivot_longer(cols = -gene, names_to = "sample", values_to = "expression") %>%
+  mutate(type="Data Normalisasi (rlog)")
+
+# 2. Gabungkan kedua data
+combined_data <- bind_rows(
+  counts_long %>% mutate(value = log2(count +1),
+                         type = "Data Mentah"),
+  rld_long %>% mutate(value = expression,
+                      type = "Data Normalisasi (rlog)")
+)
+
+# 3. Visualisasi dengan Boxplot
+library(ggplot2)
+ggplot(combined_data, aes(x=sample, y=value, fill = type)) +
+  geom_boxplot() +
+  facet_wrap(~type, scales="free_y", ncol=2 ) + 
+  theme_minimal() +
+  labs( title = "Perbandingan Distribusi Ekspresi Gen",
+        subtitle = "Data Mentah vs Data Normalisasi (rlog)",
+        x = "Sampel",
+        y = "Log2 Ekspresi") +
+  theme(axis.text.x = element_text(angle=45, hjust=1, size=8),
+        strip.text = element_text(face = "bold", size=10),
+        legend.position = "none")
+
+
+# Mission 03 : Differential Gene Expression (DEG) Analysis
+# --------------------------------------------------------
+# ======================
+# Langkah 1: DESeq2
+# ======================
+# 1. Membuat object dds
+dds <- DESeqDataSetFromMatrix(
+  countData = counts_filtered,
+  colData = metadata,
+  design = ~condition
+)
+
+# 2. Jalankan DESeq2
+dds <- DESeq(dds)
+
+# 3. Ambil hasil perbandingan
+res <- results(dds, contrast = c("condition", "DTG", "DMSO"))
+res <- res[order(res$padj),]
+head(res)
+
+# ======================
+# Langkah 2: Filter Gen
+# ======================
+# 1. Ubah data jadi format dataframe
+res_df <- as.data.frame(res)
+
+# 2. Tambahkan kolom status signifikansi
+res_df$significant <- ifelse(
+  !is.na(res_df$padj) & 
+    res_df$padj < 0.05 &
+    abs(res_df$log2FoldChange) > 1,
+  "Signifikan", 
+  "Tidak Signfikan"
+)
+
+# 3. Tambahkan kolom arah perubahan
+res_df$direction <- ifelse(
+  !is.na(res_df$padj) & res_df$padj < 0.05 & res_df$log2FoldChange >1, "Naik", 
+  ifelse(
+    !is.na(res_df$padj) & res_df$padj < 0.05 & res_df$log2FoldChange < -1, "Turun",
+    "Tidak Signifikan"))
+
+
+# 4. Filter gen signifikan
+sig_genes <- res_df[res_df$significant == "Signifikan",]
+nrow(sig_genes)
+
+# 5. Lihat gen naik dan turun
+table(sig_genes$direction)
+
+# =========================
+# Langkah 3: Export Hasil
+# =========================
+# 1. Export hasil
+write.csv(res_df, "DEG_results_GSM285595.csv")
+
+# 2. Lihat top 10 gen naik
+top_up <- head(res_df[res_df$direction == "Naik",c("log2FoldChange", "pvalue", "padj")], 10)
+print(top_up)
+
+  
+# 3. Lihat top 10 gen turun
+top_down <- head(res_df[res_df$direction == "Turun", 
+                        c("log2FoldChange", "pvalue", "padj")], 10)
+print(top_down)
+
+# Mission 04 : Visualization
+# --------------------------------------------------------
+# =========================
+# Langkah 1: Volcano Plot
+# =========================
+library(dplyr)
+library(ggplot2)
+
+# 1. Ambil 10 gen dengan padj terkecil (paling signifikan)
+top_genes <- res_df %>% 
+  filter(!is.na(padj) & padj < 0.05 & abs(log2FoldChange) > 1) %>% 
+  arrange(padj) %>%
+  head(10)
+
+# 2. Tambahkan kolom label (hanya untuk gen top)
+res_df$label <- ifelse(rownames(res_df) %in% 
+                         rownames(top_genes), rownames(res_df), "")
+
+# 3. Buat volcano plot dengan label
+#volcano_plot_labeled <- ggplot(res_df, aes(x=log2FoldChange, y = -log10(padj), color=direction))+
+  
